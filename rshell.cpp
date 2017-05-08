6c29c05f3dd0bd11b71d2ce8a56626cecfd4d940
Kau Chung <kchun014@ucr.edu>
2017-05-08 15:47:27 -0700

    Working on the implementation of connectors, setting up framework

--- rshell.cpp
+++ rshell.cpp
@@ -15,8 +15,11 @@ vector <int> parse1 ( string & input );
 int main( ) {
     bool val = true;
     string input;
-    vector < int > semicolons;
     vector < int > connectors;
+    vector < int > semicolons;
+    vector < int > orConn;
+    vector < int > andConn;
+    vector < int > comment;
     
     while (val) {
         
@@ -26,17 +29,70 @@ int main( ) {
         
         getline(cin, input);
         typedef boost::tokenizer< boost::char_separator< char > > tokenizer;
-        boost::char_separator< char > key(" ", ";#");
+        boost::char_separator< char > key(" ");//Originally separated by ; and # as well, but that would remove them, it's better to keep those in.
         tokenizer tokens(input, key);
-        
-        for (tokenizer::iterator tok_iter = tokens.begin(); tok_iter != tokens.end(); ++tok_iter) {
-            cout << "<" << *tok_iter << "> ";
+        int i = 0;
+        for (tokenizer::iterator tok_iter = tokens.begin(); tok_iter != tokens.end(); ++tok_iter, ++i) {
+            //Serves to parse the tokens further, with vectors.
+            string temp = *tok_iter;
+            //cout << "<" << *tok_iter << "> ";
+            if ( temp == "||") {
+                orConn.push_back( i );
+            }
+            if ( temp == "&&" ) {
+                andConn.push_back( i );
+            }
+            if ( temp.at( 0 ) == '#' ) {
+                comment.push_back( i );
+            }
+            if ( temp.at( temp.length() - 1 ) == ';') {
+                semicolons.push_back( i );
+            }
         }
-        tokenizer::iterator tok_iter2 = tokens.begin();
-        // while (*tok_iter != "||" && tok_iter != ";" && tok_iter != "&&" ) {
-            
-        // }
-        execvp(*tok_iter2, );
+        cout << endl;
+        
+        // test value: ls -a; #this is really lame; spaghetti && macaroni || echo
+        for(unsigned k = 0; k < orConn.size(); k++)
+        {
+            cout << "orConn values: " << orConn.at(k) << " ";
+            connectors.push_back( orConn.at( k ) );
+        }
+        
+        cout << endl;
+        for(unsigned k = 0; k < andConn.size(); k++)
+        {
+            cout << "andConn values: " << andConn.at(k) << " ";
+            connectors.push_back( andConn.at( k ) );
+        }
+        cout << endl;
+        for(unsigned k = 0; k < semicolons.size(); k++)
+        {
+            if ( k == 0 ) {
+                cout << "semicolon values: " ;
+            }
+            cout << semicolons.at(k) << " ";
+            connectors.push_back( semicolons.at( k ) );
+        }
+        cout << endl;
+        for(unsigned k = 0; k < connectors.size(); k++)
+        {
+            if ( k == 0 ) {
+                cout << "connector values: " ;
+            }
+            cout << connectors.at(k) << " ";
+        }
+        cout << endl;
+        
+        //Want to create connector vectors? or just instantiate connector pointers, based on connectors, then instantiate them as you need to. 
+        //Run connector, which takes values from 
+        
+        // tokenizer::iterator tok_iter2 = tokens.begin();
+        // advance(tok_iter2, orConn.at(0));
+        // cout << "tok_iter2 is at:" << * tok_iter2;
+        
+        //string current = *tok_iter2;
+        //strcpy ( pinput, current.c_str() );
+        //execvp((*tok_iter2), ( *tok_iter ).c_str());
     }
 }
 
