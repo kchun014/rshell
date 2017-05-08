440795a90c7b89eb9e9add3567143feeabbb8de6
Kau Chung <kchun014@ucr.edu>
2017-05-07 19:44:25 -0700

    Implemented parsing, testing more execvp

--- rshell.cpp
+++ rshell.cpp
@@ -1,14 +1,46 @@
 #include <iostream>
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
 #include <sys/wait.h>
 #include <sys/types.h>
-#include <boost/tokenizer>
 #include <string.h>
+#include <vector>
+#include <boost/tokenizer.hpp>
 
 using namespace std;
 
-int main( int argc, char * argv[] ) {
-	execvp(argv[2], argv[2:]);
-	return;
+vector <int> parse1 ( string & input );
+
+int main( ) {
+    bool val = true;
+    string input;
+    vector < int > semicolons;
+    vector < int > connectors;
+    
+    while (val) {
+        
+        if (input == "") {
+            cout << "$ ";
+        }
+        
+        getline(cin, input);
+        typedef boost::tokenizer< boost::char_separator< char > > tokenizer;
+        boost::char_separator< char > key(" ", ";#");
+        tokenizer tokens(input, key);
+        
+        for (tokenizer::iterator tok_iter = tokens.begin(); tok_iter != tokens.end(); ++tok_iter) {
+            cout << "<" << *tok_iter << "> ";
+        }
+        tokenizer::iterator tok_iter2 = tokens.begin();
+        // while (*tok_iter != "||" && tok_iter != ";" && tok_iter != "&&" ) {
+            
+        // }
+        execvp(*tok_iter2, );
+    }
+}
+
+void parse(char * check, char ** argv);
+void exit( bool & val ) {
+    val = false;
 }
