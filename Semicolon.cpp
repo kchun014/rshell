25abbf862e1886cf537727831640b1e12768b986
Kau Chung <kchun014@ucr.edu>
2017-05-09 21:23:07 -0700

    finished bugfixing, bugtesting.

--- Semicolon.cpp
+++ Semicolon.cpp
@@ -1,50 +1,50 @@
 #include <vector>
 #include <string>
+#include <iostream>
 #include "Semicolon.h"
 #include "Connector.h"
 
 bool Semicolon::execute ()  {
-    for (unsigned i = 0; i < this->args.size(); i++) {
-        //Comment case, 
-        andex = false;
-        orex = false;
-        if (this->args.at(i).at(0) == '#') {
-            return 1;//comments end the attempts at execution
+    this->orex = true;//To insure first iteration always runs
+    this->andex = false;
+    this->check = false;
+    for (unsigned i = 0; i < args.size(); i++) {
+        if ((args.at(i) != "||") && (this->args.at(i) != "&&")) {
+            this->runargs.push_back(this->args.at(i));//Push back when not an operators
         }
-        //Comment isn't true
-        if ((this->args.at(i) != "||") && (this->args.at(i) != "&&")) {
-            this->runargs.push_back(this->args.at(i));
-        }
-        
-        if ((this->args.at(i).at(this->args.at(i).length()) - 1 ) == ';') {
+        //If you find a semicolon, remove it, push back a version without, and continue, set flag to run.
+        if (args.at(i).at(args.at(i).length() - 1) == ';') {
             this->semiex = true;
             this->runargs.pop_back();
-            this->runargs.push_back(this->args.at(i).substr(0, args.at(i).length()-2));
+            this->runargs.push_back(this->args.at(i).substr(0, args.at(i).length()-1));
         }
-        
-        if (this->args.at(i) == "||" || args.at(i) == "&&" || semiex) {
-            for(unsigned i = 0; i < runargs.size(); i++) {
-                this->runargs.at(i) += "\0";
+        //Checking for when it should run.
+        if (args.at(i) == "||" || args.at(i) == "&&" || semiex || (i+1 == args.size())) {
+            for(unsigned j = 0; j < runargs.size(); j++) {//Append strings with \0 for execvp char pointers.
+                this->runargs.at(j) += '\0';
             }
-            if (semiex) {//reset flags if semicolon, and only if semicolon.
-                this->andex = false;
-                this->orex = false;
+            //Checks if conditions are true; if so, output.
+            if((orex && !check) || (andex && check) || semiex || (i+1 == args.size())) {//First boolean check, checks for finish conditions.
+                
+                if ((orex && !check) || (andex && check)) {//Secondary boolean check. First one will succeed due to initial flags.
+                    Connector * ex = new Connector(runargs);
+                    this->check = ex->execute();
+                }
             }
-            if ((orex && !check) || (andex && check) || semiex) {
-                Connector * ex = new Connector(runargs);
-                this->check = ex->execute();
+            else {
+                check = false;//False if previous didn't run.
             }
             runargs.clear();//Clear regardless, since if you don't run you don't want the commands either.
             this->semiex = false;
         }
         if (this->args.at(i) == "||") {
             this->orex = true;
-            this->andex = false;
+            //this->andex = false;
             
         }
         else if (this->args.at(i) == "&&") {
             this->andex = true;
-            this->orex = false;
+            //this->orex = false;
         }
     }
     return true;
