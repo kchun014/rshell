a1715c4779637c7c54a07659d9649ba530b44094
Kau Chung <kchun014@ucr.edu>
2017-05-28 22:46:33 -0700

    deleted all of old code, recoded shell

--- makefile
+++ makefile
@@ -4,21 +4,15 @@ EXEC=rshell
 SOURCES=$(wildcard *.cpp)
 OBJECTS=$(SOURCES:.cpp=.o)
 
-all: Rshell Executable Connector Semicolon
+all: Rshell Evaluate
 	$(CC) src/*.cpp -o ./bin/$(EXEC) #helps to make the executable.
 
 Rshell: Bin
 	$(CC) -c $(CC_FLAGS) src/Rshell.cpp -o ./bin/rshell.o
 
-Executable: Bin
-	$(CC) -c $(CC_FLAGS) src/Executable.cpp   -o ./bin/Executable.o
+Evaluate: Bin
+	$(CC) -c $(CC_FLAGS) src/Evaluate.cpp   -o ./bin/Evaluate.o
 
-Connector: Bin
-	$(CC) -c $(CC_FLAGS) src/Connector.cpp   -o ./bin/Connector.o
-
-Semicolon: Bin
-	$(CC) -c $(CC_FLAGS) src/Semicolon.cpp   -o ./bin/Semicolon.o
-	
 Bin:
 	mkdir -p bin
 	
--- src/Rshell.cpp
+++ src/Rshell.cpp
@@ -1,73 +1,49 @@
 #include <iostream>
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <sys/wait.h>
-#include <sys/types.h>
 #include <string.h>
+#include <string>
 #include <vector>
 #include <boost/tokenizer.hpp>
-#include "Executable.h"
-#include "Connector.h"
-#include "Semicolon.h"
-#include "Base.h"
+#include "Evaluate.h"
 
 using namespace std;
 
-void set_exit(bool & val);
-
 int main( ) {
-    // initialize our value(keeps the loop going)
-    //intialize input string, and vectors possessing indices of their respective symbol.
-    //ie. 'connectors' holds the index of all connectors in the input string; 'semicolons' holds the indices of all semicolons, etc.
+    //bool comment = false;
+    //initialize bool to keep loop going, user input string
+    //then place tokenized strings into a vector to be further parsed
     bool val = true;
     string input;
-    vector < string > commandsList;
-    bool commentflag;
-
+    vector<string> commands;
+    
     while (val) {
         cout << "€ ";
-        commentflag = false;//Start flag as false;
-        //continously grab line of input, parsed using boost tokenizer.
-        //define the boost::tokenizer type so we dont have to rewrite that mile long signature every single time.
+        //comment = false;
+        
         getline(cin, input);
+        // properly takes in two things.
         typedef boost::tokenizer<boost::char_separator<char> > tokenizer;
-        boost::char_separator<char> key(" \n");//Originally separated by ; and # as well, but that would remove them, it's better to keep those in.
-        tokenizer tokens(input, key);//initialize a tokenizer to convert input into tokens, separated by whitespaces.
-
-        tokenizer::iterator tok_iter = tokens.begin();
-
-        for(; tok_iter != tokens.end(); ++tok_iter)
+        boost::char_separator<char> key(" ");
+        tokenizer tokens(input, key);
+        //Create iterators to iterate through tokens.
+        tokenizer::iterator itr = tokens.begin();
+        tokenizer::iterator itrEND = tokens.end();
+        
+        for(; itr != itrEND; ++itr)
         {
-            string temp2 = *tok_iter;
-            if (temp2.at(0) != '#' && !commentflag) {
-                commandsList.push_back(temp2);
-            }
-            else {
-                commentflag = true;
-            }
-            if (temp2 == "done" || temp2 == "done;" || temp2 == "Done" || temp2 == "Done;") {
-                set_exit(val);
-                break;
-            }
-            tokenizer::iterator tok_iter2 = tok_iter;//Use second iterator to see if its the end
-            tok_iter2++;
-            //Comment case
-            if ((temp2.at(temp2.length() - 1) == ';' || tok_iter2 == tokens.end()) && val) {
-                Semicolon* sem = new Semicolon(commandsList);
-                sem->execute();
-                commandsList.clear();//Run each semicolon one at a time.
-            }
+            commands.push_back(*itr);
         }
-        commandsList.clear();
-        if (val == false) {
-            break;
+        if(commands.size() != 0)
+        {
+            Base * run = new Input(commands);
+            run->execute();
+            commands.clear();
         }
-        // test value: ls -a; #this is really lame; spaghetti && macaroni || echo
+        if(input.find("done") != string::npos) {
+            val = false;
+            
+            continue;
+        }
+        
     }
-}
-
-void set_exit(bool & val) {
-    val = false;
-    cout << "exiting rshell..." << endl;
+       
 }
